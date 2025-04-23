aa45fa2fda1693140a314f3375eaa79dac508e16
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-04-23 10:57:38 +0200

    added hello world controller and html gfile

---
+++ src/main/java/HelloWorldController.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "hello-world";
+    }
+}
