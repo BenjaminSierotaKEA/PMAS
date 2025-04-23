a0b009a3d05911e827188c86e8af093bf7263477
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-04-23 10:45:22 +0200

    Initial Commit

---
+++ src/test/java/org/example/pmas/PmasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
