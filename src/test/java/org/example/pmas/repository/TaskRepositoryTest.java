9306eed39766e6edc82cd6839e2db046d157ea79
mathiasepc <mathiasepc@msn.com>
2025-05-01 23:04:35 +0200

    lavet repo test

--- src/test/java/org/example/pmas/repository/TaskRepositoryTest.java
+++ src/test/java/org/example/pmas/repository/TaskRepositoryTest.java
@@ -1,5 +1,8 @@
 package org.example.pmas.repository;
 
+import org.example.pmas.model.Task;
+import org.example.pmas.modelBuilder.MockModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,20 +10,35 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Sql(
-
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+        scripts ={"classpath:h2init.sql"}
 )
 @Transactional
 @Rollback
 class TaskRepositoryTest {
-
     @Autowired
     private TaskRepository taskRepository;
 
+    @BeforeEach
+    void setUp() {
+
+    }
+
     @Test
     void readAll() {
+        // Arrange
+        int expectedSize = 3;
+
+        // Act
+        var actualSize = taskRepository.readAll();
+
+        // Assert
+        assertEquals(expectedSize, actualSize.size());
     }
 }
