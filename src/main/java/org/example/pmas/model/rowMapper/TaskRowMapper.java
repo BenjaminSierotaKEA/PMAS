9864a20c24ab8807d73d619b07e113794ee9c931
mathiasepc <mathiasepc@msn.com>
2025-05-02 12:40:18 +0200

    add subproject to rowmapper

--- src/main/java/org/example/pmas/model/rowMapper/TaskRowMapper.java
+++ src/main/java/org/example/pmas/model/rowMapper/TaskRowMapper.java
@@ -1,5 +1,6 @@
 package org.example.pmas.model.rowMapper;
 
+import org.example.pmas.model.SubProject;
 import org.example.pmas.model.Task;
 import org.example.pmas.model.User;
 import org.springframework.jdbc.core.RowMapper;
@@ -19,6 +20,10 @@ public class TaskRowMapper implements RowMapper<Task> {
         task.setCompleted(rs.getBoolean("completed"));
         task.setUsers(mapUsers(rs));
 
+        // If subproject is in query
+        if(rs.findColumn("subproject_name") > 0)
+            task.setSubProject(mapSubproject(rs));
+
         return task;
     }
 
@@ -26,12 +31,15 @@ public class TaskRowMapper implements RowMapper<Task> {
     private Set<User> mapUsers(ResultSet rs) throws SQLException {
         Set<User> users = new HashSet<>();
 
+        // Names are concatenated with ','
         String username = rs.getString("user_names");
         String[] usernames = username.split(",");
 
+        // Users are concatenated with ','
         String id = rs.getString("user_ids");
         String[] ids = id.split(",");
 
+        // adds a user to users
         for (int i = 0; i < ids.length; i++) {
             User user = new User();
             user.setId(Integer.parseInt(ids[i]));
@@ -40,5 +48,12 @@ public class TaskRowMapper implements RowMapper<Task> {
         }
 
         return users;
+    }
+
+    private SubProject mapSubproject(ResultSet rs) throws SQLException {
+        SubProject subproject = new SubProject();
+        subproject.setName(rs.getString("subproject_name"));
+
+        return subproject;
     }
 }
