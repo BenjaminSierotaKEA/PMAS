8c6b69bbd9fe3eb98f03722a205e25b0eeb2571f
mathiasepc <mathiasepc@msn.com>
2025-05-06 22:47:14 +0200

    Kan ikke se hvorfor den fejlede

--- src/main/java/org/example/pmas/repository/Interfaces/CrudInterface.java
+++ src/main/java/org/example/pmas/repository/Interfaces/CrudInterface.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public interface CrudInterface<T> {
 
-public T create(T t);
+T create(T t);
 
-public List<T> readAll();
+List<T> readAll();
 
-public T readSelected(int id);
+T readSelected(int id);
 
-public boolean delete(int id);
+boolean delete(int id);
 
-public boolean update(T newObject);
+boolean update(T newObject);
 
 }
 
