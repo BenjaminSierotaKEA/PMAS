0bfb14cb63eab56f8dae097cf719a39df429f7f1
J-AGGERHOLM <jajo0004@stud.kea.dk>
2025-05-14 13:20:41 +0200

    link for task row on userpage fixed

--- src/main/java/org/example/pmas/repository/Interfaces/IUserRepository.java
+++ src/main/java/org/example/pmas/repository/Interfaces/IUserRepository.java
@@ -1,8 +1,12 @@
 package org.example.pmas.repository.Interfaces;
 
 import org.example.pmas.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface IUserRepository extends CrudInterface<User> {
 
     public User getByEmail(String email);
+
+    @Transactional
+    int getProjectIDOfUsersSubproject(int userID, int subprojectID);
 }
