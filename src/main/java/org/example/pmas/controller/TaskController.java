e8029079d8f963abf7f249ef9b00ba42faa83778
mathiasepc <mathiasepc@msn.com>
2025-05-21 17:10:11 +0200

    Fjernet unused import i task controller

--- src/main/java/org/example/pmas/controller/TaskController.java
+++ src/main/java/org/example/pmas/controller/TaskController.java
@@ -8,7 +8,6 @@ import org.example.pmas.util.SessionHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Set;
 
