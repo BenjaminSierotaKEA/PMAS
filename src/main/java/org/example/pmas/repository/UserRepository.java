3885a20aa69f80079b30e319f4d386d7526ede28
HastingsJack <joha0005@stud.kea.dk>
2025-05-27 09:21:54 +0200

    fixed tests and comments

--- src/main/java/org/example/pmas/repository/UserRepository.java
+++ src/main/java/org/example/pmas/repository/UserRepository.java
@@ -24,7 +24,6 @@ public class UserRepository implements IUserRepository {
 
     }
 
-
     @Override
     @Transactional
     public User create(User newUser) throws DatabaseException {
@@ -146,25 +145,6 @@ public class UserRepository implements IUserRepository {
 
         return users.isEmpty() ? null : users.get(0);
     }
-
-    @Transactional
-    @Override
-    public int getProjectIDOfUsersSubproject(int userID, int subprojectID) throws DataAccessException {
-        String sql = """
-                    SELECT sp.projectID
-                    FROM usertasks ut
-                    JOIN tasks t ON ut.taskID = t.id
-                    JOIN subprojects sp ON t.subProjectID = sp.id
-                    WHERE ut.userID = ? AND sp.id = ?
-                    LIMIT 1
-                """;
-
-        Integer result = jdbcTemplate.queryForObject(sql, Integer.class, userID, subprojectID);
-
-        return result != null ? result : 0;
-
-    }
-
 }
 
 
